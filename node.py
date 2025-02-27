7d72bcd28845ce7bc28ad9f2f25d1868a3da42af
soleilzero <mbotello@unal.edu.co>
2025-02-27 11:19:35 +0100

    Created and used new set neighbor of my neighbor functions

--- node.py
+++ node.py
@@ -19,13 +19,19 @@ class Node:
         self.left_neighbor = left_neighbor
         self.right_neighbor = right_neighbor
 
+    def set_left_neighbor_of_right_neighbor(self, other_node):
+        self.right_neighbor.set_left_neighbor(other_node)
+
+    def set_right_neighbor_of_left_neighbor(self, other_node):
+        self.left_neighbor.set_right_neighbor(other_node)
+
     def join(self, other_node):
         """The node joins the ring by connecting to an existing node."""
         print(f"Node {self.node_id} is joining the ring.")
         # The node connects to the node on the right of the other node
         self.set_neighbors(other_node, other_node.right_neighbor)
         # Updates the neighbors of the other nodes
-        other_node.right_neighbor.left_neighbor = self
+        other_node.set_left_neighbor_of_right_neighbor(self)
         other_node.set_right_neighbor(self)
         print(f"Node {self.node_id} joined between Node {other_node.node_id} and Node {other_node.right_neighbor.node_id}.")
 
@@ -33,8 +39,8 @@ class Node:
         """The node leaves the ring and updates its neighbors."""
         if self.left_neighbor and self.right_neighbor:
             print(f"Node {self.node_id} is leaving the ring.")
-            self.left_neighbor.right_neighbor = self.right_neighbor
-            self.right_neighbor.left_neighbor = self.left_neighbor
+            self.set_right_neighbor_of_left_neighbor(self.right_neighbor)
+            self.set_left_neighbor_of_right_neighbor(self.left_neighbor)
             print(f"Node {self.node_id} left. Node {self.left_neighbor.node_id} is now connected to Node {self.right_neighbor.node_id}.")
         else:
             print(f"Node {self.node_id} has no neighbors to leave.")
