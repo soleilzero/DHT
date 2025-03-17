07931347f467f2e6b5363c7016ae468d3bef5d80
soleilzero <mbotello@unal.edu.co>
2025-03-17 09:01:08 +0100

    refactor: Put the main code in functions

--- main.py
+++ main.py
@@ -2,39 +2,50 @@ import simpy
 from node import Node
 from ring import Ring
 
-# Simulation
-env = simpy.Environment()
-ring = Ring(env)
 
-# Creating and adding nodes to the ring
-node1 = Node(env, 100)
-node2 = Node(env, 250)
-node3 = Node(env, 500)
-node4 = Node(env, 750)
-node5 = Node(env, 1000)
+def main():
 
-ring.add_node(node1)
-ring.add_node(node2)
-ring.add_node(node3)
-ring.add_node(node4)
-ring.add_node(node5)
+    # Simulation
+    env = simpy.Environment()
+    ring = Ring(env)
 
-# Test: Display the routing tables of all nodes
-for node in ring.nodes:
-    print(f"Node {node.node_id} routing table: {node.routing_table}")
+    # Creating and adding nodes to the ring
+    node1 = Node(env, 100)
+    node2 = Node(env, 250)
+    node3 = Node(env, 500)
+    node4 = Node(env, 750)
+    node5 = Node(env, 1000)
 
-# Store data
-node1.put_data("user1", {"prenom": "Daniel", "x": "123", "y": "123"})
-node1.put_data("user2", {"prenom": "Sol", "x": "123", "y": "123"})
+    ring.add_node(node1)
+    ring.add_node(node2)
+    ring.add_node(node3)
+    ring.add_node(node4)
+    ring.add_node(node5)
 
-# Retrieve data
-print(node1.get_data("user1"))
-print(node1.get_data("user2"))
+    display_routing_table_of_all_nodes_in(ring.nodes)
 
-# Test: Send a message from node1 to node5
-print("\nSending message from Node 100 to Node 1000:")
-message = type('Message', (object,), {"content": "Hello from Node 100!", "receiver_id": 1000})()
-node1.route_message(message)
+    # Store data
+    node1.put_data("user1", {"prenom": "Daniel", "x": "123", "y": "123"})
+    node1.put_data("user2", {"prenom": "Sol", "x": "123", "y": "123"})
 
-# Run the simulation
-env.run(until=100)
+    # Retrieve data
+    print(node1.get_data("user1"))
+    print(node1.get_data("user2"))
+
+    # Test: Send a message from node1 to node5
+    print("\nSending message from Node 100 to Node 1000:")
+    message = type('Message', (object,), {"content": "Hello from Node 100!", "receiver_id": 1000})()
+    node1.route_message(message)
+
+    # Run the simulation
+    env.run(until=100)
+
+
+def display_routing_table_of_all_nodes_in(nodes):
+    print("\n\n#----------> Display routing table:")
+    for node in nodes:
+        print(f"Node {node.node_id} routing table: {node.routing_table}")
+
+
+if __name__ == "__main__":
+    main()
